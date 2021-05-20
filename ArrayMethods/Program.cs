8ce42c6f784c24c2603843fc5c24ad0b8e4e4aea
James Tiu <jamestiulawyer@gmail.com>
2021-05-20 18:54:17 -0400

    Array methods

--- ArrayMethods/Program.cs
+++ ArrayMethods/Program.cs
@@ -22,9 +22,19 @@ class Demo
         arrList.Add(45);
         arrList.Add(78);
         Console.WriteLine(arrList.Count);
-
         Console.WriteLine("myArrayList.IsFixedSize = " + arrList.IsFixedSize);
         Console.ReadLine();
+
+        //The Array.Lenth property in C# is used to find the length of the array.
+        Array arr = Array.CreateInstance(typeof(String), 3);
+        arr.SetValue("Electronics", 0);
+        arr.SetValue("Clothing", 1);
+        arr.SetValue("Appliances", 2);
+
+        Console.WriteLine("Length: {0}", arr.Length.ToString());
+
+        Console.ReadLine();
+
         //Creates and initializes a new integer array and a new Object array.
 
     }
